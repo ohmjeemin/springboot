944a56d4aa3314bd193f0614fb77344104dd31bc
ohmjeemin <ohmjeemin@gmail.com>
2021-07-05 23:08:08 +0900

    member controller 수정

--- src/main/java/ojm/hellospring/controller/MemberController.java
+++ src/main/java/ojm/hellospring/controller/MemberController.java
@@ -1,23 +1,23 @@
-package ojm.hellospring.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import ojm.hellospring.service.MemberService;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class MemberController {
-
-    private final MemberService memberService;
-
-    @Autowired
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
-
-    @GetMapping("/members/new")
-    public String createForm() {
-        return "members/createMemberForm";
-    }
-
-}
+//package ojm.hellospring.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import ojm.hellospring.service.MemberService;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class MemberController {
+//
+//    private final MemberService memberService;
+//
+//    @Autowired
+//    public MemberController(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
+//
+//    @GetMapping("/members/new")
+//    public String createForm() {
+//        return "members/createMemberForm";
+//    }
+//
+//}
